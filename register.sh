ae97e3308b14960c3c0724ddc7caa93f286e60b6
Till Witt <mail@tillwitt.de>
2020-03-26 16:44:59 +0100

    cleaning up registry

--- register.sh
+++ register.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 URL="$(lt -h "http://serverless.social" -p 80 --local-host www)"
-
-#URL="$(lt -h "http://serverless.social" -p 80 --local-host www --subdomain yourCustomerSubdomain)"
-
-
 wget echo "${URL}"
