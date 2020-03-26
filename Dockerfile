39ee7d8fcca3c25e7ad3ca60e65a85a40cbee4cb
Till Witt <mail@tillwitt.de>
2020-03-26 17:27:25 +0100

    Container is now registring to scratch website

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM node
 RUN npm install -g localtunnel
+WORKDIR /app
 ADD register.sh ./
 RUN chmod 755 register.sh
-ENTRYPOINT ./register.sh
+CMD ./register.sh
--- register.sh
+++ register.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-URL="$(lt -h "http://serverless.social" -p 80 --local-host www)"
-wget echo "${URL}"
+echo registring
+lt -h "http://serverless.social" -p 80 --local-host www > url.txt &
+
+# here we should rather check when the file has a certain size
+sleep 5
+line=$(head -n 1 url.txt)
+
+# registring to our scratch portal
+URL=${line:20}
+wget https://nodered.consider-iot.com/scratch/add/$URL > /dev/null
+
+# keep the container running
+tail -f /dev/null
