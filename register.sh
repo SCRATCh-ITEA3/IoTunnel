4f895878a98096825e8b5714f76135381c09545a
Till Witt <tlwt@users.noreply.github.com>
2020-10-22 15:29:56 +0200

    Update register.sh

--- register.sh
+++ register.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo registring
-lt -h "https://dyn.consider-iot.com/" -p 8080 --local-host www > url.txt &
+#lt -h "https://dyn.consider-iot.com/" -p 8080 --local-host www > url.txt &
+lt -p 8080 --local-host www > url.txt &
+
 
 # here we should rather check when the file has a certain size
 sleep 5
